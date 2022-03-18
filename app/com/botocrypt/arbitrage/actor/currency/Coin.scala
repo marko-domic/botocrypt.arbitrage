c14bba807b128d20be0fe838764510775c6b29ce
Marko Domić <marko.domic@outlook.com>
2022-03-18 15:39:12 +0100

    Finished with implementation of finding arbitrage opportunity paths in coin network

--- app/com/botocrypt/arbitrage/actor/currency/Coin.scala
+++ app/com/botocrypt/arbitrage/actor/currency/Coin.scala
@@ -1,29 +1,31 @@
 package com.botocrypt.arbitrage.actor.currency
 
-import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import com.botocrypt.arbitrage.actor.notification.Informer
+import com.botocrypt.arbitrage.actor.notification.Informer.OpportunityAlert
 import com.botocrypt.arbitrage.util.CoinIdentity
 import play.api.Logger
 
-import scala.collection.mutable
+import scala.collection.immutable.ListMap
+import scala.util.control.Breaks.{break, breakable}
 
 object Coin {
 
   sealed trait Update
 
-  case class PriceUpdate(landingCoinBaseId: String, price: Double, quantity: Double) extends Update
+  case class PriceUpdate(landingCoinBaseId: String, price: Double, amount: Double) extends Update
 
   case class PairActorUpdate(coinId: String, coinPairActor: ActorRef[Update])
     extends Update
 
-  case class Arbitrage(path: mutable.LinkedHashMap[String, ConvertedContext],
-                       exchangeToExchange: Boolean) extends Update
+  case class Arbitrage(path: ListMap[String, ConvertedContext], exchangeToExchange: Boolean, fromCoin: ConvertedContext)
+    extends Update
 
   case class PoisonPill() extends Update
 
-  case class ConversionData(landingCoinBaseId: String, landingCurrency: ActorRef[Update],
-                            exchange: String, commissions: (Double, Double))
+  case class ConversionData(landingCoinBaseId: String, landingCoin: ActorRef[Update], exchange: String,
+                            commissions: (Double, Double))
 
   case class ConvertedContext(coinBaseId: String, exchange: String, amount: Double)
 
@@ -31,18 +33,16 @@ object Coin {
             pairConversionData: Map[String, Coin.ConversionData],
             informer: ActorRef[Informer.OpportunityAlert]): Behavior[Update] =
     Behaviors.setup {
-      context =>
-        new Coin(context, coinBaseId, exchange, pairPrices, pairConversionData, informer)
-          .apply()
+      _ => new Coin(coinBaseId, exchange, pairPrices, pairConversionData, informer).apply()
     }
 }
 
-class Coin private(context: ActorContext[Coin.Update],
-                   coinBaseId: String,
-                   exchange: String,
-                   var pairPrices: Map[String, Double],
-                   var pairConversionData: Map[String, Coin.ConversionData],
-                   informer: ActorRef[Informer.OpportunityAlert]) {
+class Coin private(
+                    coinBaseId: String,
+                    exchange: String,
+                    var pairPrices: Map[String, Double],
+                    var pairConversionData: Map[String, Coin.ConversionData],
+                    informer: ActorRef[Informer.OpportunityAlert]) {
 
   import Coin._
 
@@ -53,11 +53,13 @@ class Coin private(context: ActorContext[Coin.Update],
   protected def apply(): Behavior[Update] = Behaviors.receiveMessage {
     case coinPairActorUpdate: PairActorUpdate => setCoinPairActor(coinPairActorUpdate)
     case priceUpdate: PriceUpdate => updateCoinPrice(priceUpdate)
+    case arbitrage: Arbitrage => findOpportunity(arbitrage)
     case _: PoisonPill => Behaviors.stopped
   }
 
   private def setCoinPairActor(coinPairActorUpdate: PairActorUpdate): Behavior[Update] = {
-    logger.trace("SetCoinPairActor message received.")
+
+    logger.trace(s"SetCoinPairActor message received. Coin id: ${coinPairActorUpdate.coinId}")
 
     val updateCoinId = coinPairActorUpdate.coinId
     val updateCoinActor = coinPairActorUpdate.coinPairActor
@@ -68,7 +70,7 @@ class Coin private(context: ActorContext[Coin.Update],
       return Behaviors.same
     }
 
-    if (landingConversionData.landingCurrency == null) {
+    if (landingConversionData.landingCoin == null) {
       logger.info(s"Setting coin pair ${CoinIdentity.getCoinId(coinBaseId, exchange)} - " +
         s"$updateCoinId from exchange $exchange to exchange ${landingConversionData.exchange}.")
 
@@ -84,26 +86,145 @@ class Coin private(context: ActorContext[Coin.Update],
 
   private def updateCoinPrice(priceUpdate: PriceUpdate): Behavior[Update] = {
 
+    logger.trace(s"Coin price update message received. Landing coin: ${priceUpdate.landingCoinBaseId}, " +
+      s"price: ${priceUpdate.price}, amount: ${priceUpdate.amount}")
+
     val landingCoinBaseId = priceUpdate.landingCoinBaseId
     if (pairPrices.contains(landingCoinBaseId)) {
       pairPrices += landingCoinBaseId -> priceUpdate.price
 
+      val sourceAmount: Double = priceUpdate.amount
       val startingCoinId: String = CoinIdentity.getCoinId(coinBaseId, exchange)
-      val statingContext: ConvertedContext = ConvertedContext(coinBaseId, exchange,
-        priceUpdate.quantity)
-      val startingPath: mutable.LinkedHashMap[String, ConvertedContext] =
-        mutable.LinkedHashMap(startingCoinId -> statingContext)
-      arbitrageInsideExchange(landingCoinBaseId, startingPath)
+      val statingContext: ConvertedContext = ConvertedContext(coinBaseId, exchange, sourceAmount)
+      val startingPath: ListMap[String, ConvertedContext] = ListMap(startingCoinId -> statingContext)
+      arbitrageInsideExchange(landingCoinBaseId, sourceAmount, startingPath)
     }
 
     Behaviors.same
   }
 
-  private def arbitrageInsideExchange(landingCoinBaseId: String,
-                                      arbitragePath: mutable.LinkedHashMap[String, ConvertedContext]): Unit = {
+  private def findOpportunity(arbitrage: Arbitrage): Behavior[Update] = {
 
-    val landingCoinId: String = CoinIdentity.getCoinId(landingCoinId, exchange)
-    val landingConversionData: ConversionData = pairConversionData(landingCoinId)
-    val landingQuantity =
+    logger.trace(s"Find arbitrage path message received. Current path: ${arbitrage.path}, " +
+      s"from coin: ${arbitrage.fromCoin.coinBaseId}, from exchange: ${arbitrage.fromCoin.exchange}")
+
+    val path: ListMap[String, ConvertedContext] = arbitrage.path
+    val sourceAmount = path.last._2.amount
+    val receivedFromExchange = arbitrage.exchangeToExchange
+    val originCoinId = CoinIdentity.getCoinId(arbitrage.fromCoin.coinBaseId, arbitrage.fromCoin.exchange)
+
+    // Search for opportunity inside the same exchange
+    for ((landingCoinBaseId: String, _: Double) <- pairPrices) {
+      breakable {
+        val landingCoinId = CoinIdentity.getCoinId(landingCoinBaseId, exchange)
+
+        // Check if landing coin is on path in the same exchange and not from previous coin
+        if (path.contains(landingCoinId) && landingCoinId != originCoinId) {
+          validateOpportunity(path(landingCoinId), sourceAmount, path)
+          break()
+        }
+
+        // Check if landing coin is path but in other exchanges
+        if (pathContainsCoin(path, landingCoinBaseId) && !receivedFromExchange) {
+          validateOpportunity(findConvertedContextInPath(path, landingCoinBaseId), sourceAmount, path)
+          break()
+        }
+
+        // If nothing is found, proceed with finding arbitrage path
+        arbitrageInsideExchange(landingCoinBaseId, sourceAmount, path)
+      }
+    }
+
+    // Proceed with arbitrage requests on other exchanges
+    if (!receivedFromExchange) {
+      arbitrageWithOtherExchanges(sourceAmount, path)
+    }
+
+    Behaviors.same
+  }
+
+  private def arbitrageInsideExchange(landingCoinBaseId: String, sourceAmount: Double,
+                                      arbitragePath: ListMap[String, ConvertedContext]): Unit = {
+    val landingCoinId: String = CoinIdentity.getCoinId(landingCoinBaseId, exchange)
+    val landingContext: ConvertedContext = ConvertedContext(landingCoinBaseId, exchange,
+      calculateLandingAmountInsideExchange(landingCoinId, landingCoinBaseId, sourceAmount))
+    val landingPath: ListMap[String, ConvertedContext] = arbitragePath + (landingCoinId -> landingContext)
+    pairConversionData(landingCoinId).landingCoin ! Arbitrage(landingPath, exchangeToExchange = false,
+      ConvertedContext(coinBaseId, exchange, -1))
+  }
+
+  private def arbitrageWithOtherExchanges(sourceAmount: Double,
+                                          arbitragePath: ListMap[String, ConvertedContext]): Unit = {
+
+    for ((landingCoinId: String, conversionData: Coin.ConversionData) <- pairConversionData) {
+      val landingCoinBaseId: String = conversionData.landingCoinBaseId
+      val landingExchange: String = conversionData.exchange
+      if (landingCoinBaseId == coinBaseId) {
+        val landingAmount = calculateLandingAmountOnOtherExchange(landingCoinId, sourceAmount)
+        val convertedContext: ConvertedContext = ConvertedContext(landingCoinBaseId, landingExchange, landingAmount)
+        val landingPath: ListMap[String, ConvertedContext] = arbitragePath + (landingCoinId -> convertedContext)
+        conversionData.landingCoin ! Arbitrage(landingPath, exchangeToExchange = true,
+          ConvertedContext(coinBaseId, exchange, -1))
+      }
+    }
+  }
+
+  private def calculateLandingAmountInsideExchange(landingCoinId: String, landingCoinBaseId: String,
+                                                   sourceAmount: Double): Double = {
+
+    val landingPrice: Double = pairPrices(landingCoinBaseId)
+    val landingAmount: Double = sourceAmount * landingPrice
+    val commissions: (Double, Double) = pairConversionData(landingCoinId).commissions
+    val calculatedCommission: Double = (landingAmount * (commissions._1 / 100)) + commissions._2
+
+    landingAmount - calculatedCommission
+  }
+
+  private def calculateLandingAmountOnOtherExchange(landingCoinId: String, amount: Double): Double = {
+
+    val commissions: (Double, Double) = pairConversionData(landingCoinId).commissions
+    amount - ((amount * (commissions._1 / 100)) + commissions._2)
+  }
+
+  private def pathContainsCoin(arbitragePath: ListMap[String, ConvertedContext], landingCoinBaseId: String): Boolean = {
+    findConvertedContextInPath(arbitragePath, landingCoinBaseId) != null
+  }
+
+  private def findConvertedContextInPath(arbitragePath: ListMap[String, ConvertedContext],
+                                         landingCoinBaseId: String): ConvertedContext = {
+
+    // Find converted context in arbitrage path for landing coin
+    for ((_: String, landingConvertedContext: ConvertedContext) <- arbitragePath) {
+      if (landingConvertedContext.coinBaseId == landingCoinBaseId) {
+        return landingConvertedContext
+      }
+    }
+
+    // Return null if context not found
+    null
+  }
+
+  private def validateOpportunity(landingConvertedContext: ConvertedContext, sourceAmount: Double,
+                                  path: ListMap[String, ConvertedContext]): Unit = {
+
+    val landingCoinBaseId: String = landingConvertedContext.coinBaseId
+    val landingCoinId: String = CoinIdentity.getCoinId(landingCoinBaseId, exchange)
+    val newLandingAmount = calculateLandingAmountInsideExchange(landingCoinId, landingCoinBaseId, sourceAmount)
+
+    // Check if new amount is greater then existing amount in arbitrage path
+    if (newLandingAmount > landingConvertedContext.amount) {
+      logger.info("Arbitrage opportunity found")
+      informer ! convertPathToOpportunityAlert(path)
+    }
+  }
+
+  private def convertPathToOpportunityAlert(path: ListMap[String, ConvertedContext]): OpportunityAlert = {
+
+    val opportunityPath: List[Informer.CoinContext] = path.map {
+      case (_: String, convertedContext: ConvertedContext) =>
+        Informer.CoinContext(convertedContext.coinBaseId, convertedContext.exchange)
+    }.toList
+
+    OpportunityAlert(opportunityPath)
   }
 }
